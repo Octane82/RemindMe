d81925fa63f8e83751fbeeaca93ce6c88d799182
Octane_cadcam <octanek@hotmail.com>
2015-06-04 16:12:40 +0400

    Add first Activity - MainActivity (ITS - 4.06.2015)

---
+++ app/src/main/java/ru/sample/remindme/MainActivity.java
@@ -0,0 +1,21 @@
+package ru.sample.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ITS on 04.06.2015.
+ * http://habrahabr.ru/post/259429/
+ *
+ * http://www.youtube.com/channel/UCORRUYUmW1pffMgLPzU0XCA  -   devColibri youTube
+ * https://github.com/google/material-design-icons
+ */
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
